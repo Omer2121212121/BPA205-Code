5b6c521a9e83084aeb3d9052b91a5feea957a138
Omer2121212121 <mirzeyevomer1@gmail.com>
2026-03-05 13:44:19 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Net.Mime;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ConsoleApp3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Task 1 
+        ////float orta = 0;
+        ////int say = 0;
+        ////int cem = 0;
+        ////int m = int.Parse(Console.ReadLine());
+        ////for (int i = 0; i <= m; i++)
+        ////{
+        ////    if (i % 15 == 0)
+        ////    {
+        ////        cem =cem + i;
+        ////        say++;
+        ////    }
+        ////}
+        //Task 2 
+        ////orta = cem/say;
+        //////Console.WriteLine(orta);
+        ////int n = int.Parse(Console.ReadLine());
+        ////for (int i = n; i < 100; i++)
+        ////{
+        ////    if (i % 21 == 0)
+        ////    {
+        ////        Console.WriteLine(i);
+        ////    }
+
+        ////}
+        //Task 3
+        ////int n = int.Parse(Console.ReadLine());
+        ////int reqem = 0;
+        ////while (n > 0) 
+        ////{
+        ////    reqem = reqem + n%10;
+        ////    n = n / 10;
+        ////}
+        ////Console.WriteLine(reqem);
+        //Task 4
+        //int[] arr = [2, 3, 4, 5, 6, 7];
+        //int vurma = 1;
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    if (arr[i] % 2 == 0)
+        //    {
+        //        vurma = arr[i] * vurma;
+        //    }
+        //}
+        //Console.WriteLine(vurma);     
+        //Task 5
+        //int[] arr = [2, 3, 4, 5, 6, 7];
+        //int cem = 0;
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    if (arr[i] % 2 == 0)
+        //    {
+        //        cem = arr[i] + cem;
+        //    }
+        //}
+        //Console.WriteLine(cem);
+        //Task 6
+        //int eded = int.Parse(Console.ReadLine());
+        //int fakt = 1;
+        //for (int i = 1; i < eded; i++)
+        //{
+        //    fakt = fakt*(i+1);
+        //}
+        ////Console.WriteLine(fakt);
+        //Task 7
+        //int n = int.Parse(Console.ReadLine());
+        //int hasil = 1;
+        //while (n > 0)
+        //{
+        //    hasil = hasil * n % 10;
+        //    n = n / 10;
+        //}
+        //Console.WriteLine(hasil);
+        //Task 8
+        //int n = int.Parse(Console.ReadLine());
+        //int a = 0;
+        //int b = 1;
+        //int daxil = 0;
+        //while (a  <= n) {
+        //    Console.WriteLine(a);
+        //    daxil = a + b;
+        //    a = b;
+        //    b = daxil;
+        //}
+        //Task 9
+        //int[] arr = [1, 2, 3, 4, 5, 6, 7, 8];
+        //int a = arr.Length / 2;
+        //float median = 0;
+        //if (a % 2 == 1)
+        //{
+        //    Console.WriteLine(arr[a]);
+        //}
+        //else
+        //{
+        //    median = (arr[a - 1] + arr[a + 1]) / 2;
+        //    Console.WriteLine(median);
+        //}
+        //Task 10
+        //int[] arr = [1, 2, 3, 4, 5, 6, 7, 8];
+        //int max = arr[0];
+        //int min = arr[0];
+        //float median = 0;
+        //for (int i = 0; i < arr.Length; i++)
+        //{
+        //    if (max < arr[i])
+        //    {
+        //        max = arr[i];
+        //    }
+        //    else if (min > arr[i])
+        //    {
+        //        min = arr[i];
+        //    }
+        //}
+        //Console.WriteLine(max);
+        //Console.WriteLine(min);
+        //Sert operatoru isletmeden tek veya cut oldugunu bilmey 
+        //string[] arr = ["Cutdur", "Tekdir"];
+        //int n = int.Parse(Console.ReadLine());
+        //Console.Write(arr[n % 2]);
+    }
+}
+
+
