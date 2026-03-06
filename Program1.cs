4350e1337f30b7ff6cd7a43a570687a973fc4eda
Omer2121212121 <mirzeyevomer1@gmail.com>
2026-03-06 18:00:15 +0400

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,79 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parametr olaraq verilmis ededin reqemleriin cemii 10-dursa "bingo", deyilse reqemleri cemini cap etdirin
+            //int n = int.Parse(Console.ReadLine());
+            //int cem = 0;
+            //while (n > 0)
+            //{
+            //    cem = cem + n % 10;
+            //    n = n / 10;
+            //}
+            //if(cem == 10)
+            //{
+            //    Console.WriteLine("bingo");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(cem);
+            //}
+            //1den n e qeder ededleri cap edin;3e bolunen - "fizz";5e bolunen - "buzz";herikisinie bolunen ise "fizbuzz"
+            //int n = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    if (i % 3 != 0 && i % 5 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    else if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        Console.WriteLine("fizzbuzz");
+            //    }
+            //    else if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine("buzz");
+            //    }
+            //    else if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine("fizz");
+            //    }
+            //}
+            //Verilmis stringdeki butun herfleri cap edin amma ilk samitler sonra saitler olmalidir
+            //Console.WriteLine("Sozu daxil edin: ");
+            //string word = Console.ReadLine();
+
+            //char[] sait = ['a', 'e', 'i', 'o', 'u'];
+            //char[] samit = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+            //string temp = "";
+
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    foreach (char item in samit)
+            //    {
+            //        if (word[i] == item)
+            //        {
+            //            temp = temp + item;
+            //        }
+            //    }
+            //}
+
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    foreach (char item in sait)
+            //    {
+            //        if (word[i] == item)
+            //        {
+            //            temp = temp + item;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine(temp); 
+        }
+    }
+}
